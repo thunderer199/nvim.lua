a2346b354cf49b1fd13173212d259bb7e27b6291
Vlad Lashko <v.lashko@yahoo.com>
2023-01-05 21:49:48 +0200

    copy json path command

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -25,3 +25,20 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
+
+local ts_utils = require 'nvim-treesitter.ts_utils'
+local function get_json_path()
+  local result = {}
+  local node = ts_utils.get_node_at_cursor()
+  while node do
+    if tostring(node) == '<node pair>' then
+      local key_node = node:named_child(0):named_child(0)
+      table.insert(result, 1, ts_utils.get_node_text(key_node)[1])
+    end
+    node = node:parent()
+  end
+  print(vim.fn.join(result, '.'))
+  -- copy to clipboard
+  vim.fn.setreg('+', vim.fn.join(result, '.'))
+end
+vim.keymap.set('n', '<leader>nn', get_json_path)
