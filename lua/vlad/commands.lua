a7f47b604483ea689f4a3aaca8d12766367933bd
Vlad Lashko <v.lashko@yahoo.com>
2023-01-05 22:26:53 +0200

    angular files jumper

--- lua/vlad/commands.lua
+++ lua/vlad/commands.lua
@@ -5,3 +5,32 @@ vim.api.nvim_create_user_command("Cppath", function()
 end, {})
 
 vim.keymap.set('n', '<leader>cp', ':Cppath<CR>')
+
+
+-- Angular files jump
+vim.api.nvim_create_user_command("A", function(opts)
+    local type = opts.args
+    local path = vim.fn.expand("%:t:r")
+    local main_path = path
+    if path:find(".spec") then
+        main_path = path:gsub(".spec", "")
+    end
+
+    if type == 'spec' then
+        vim.fn.execute(":find " .. main_path .. ".spec.ts")
+    elseif type == 'main' then
+        vim.fn.execute(":find " .. main_path .. ".ts")
+    elseif type == 'scss' then
+        vim.fn.execute(":find " .. main_path .. ".scss")
+    elseif type == 'html' then
+        vim.fn.execute(":find " .. main_path .. ".html")
+    end
+end, { nargs = 1 })
+-- go to spec file
+vim.keymap.set('n', '<leader>at', ':A spec<CR>')
+-- got to componrny file
+vim.keymap.set('n', '<leader>am', ':A main<CR>')
+-- go to scss file
+vim.keymap.set('n', '<leader>as', ':A scss<CR>')
+-- go to html file
+vim.keymap.set('n', '<leader>ah', ':A html<CR>')
