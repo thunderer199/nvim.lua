d721142e692e18e9e375d655025026184b6d54d2
Vlad Lashko <v.lashko@yahoo.com>
2023-01-08 18:00:29 +0200

    some fast commands to save and quit

--- lua/vlad/remap.lua
+++ lua/vlad/remap.lua
@@ -44,3 +44,5 @@ vim.keymap.set("n", "<leader>bp", "<cmd>bp<CR>")
 
 -- save file faster
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
+vim.keymap.set('n', '<leader>q', '<cmd>q<CR>')
+vim.keymap.set('n', '<leader>x', '<cmd>x<CR>')
