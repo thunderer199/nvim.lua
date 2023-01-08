cca9f7c615be7ea880a08329490149b2c5294970
Vlad Lashko <v.lashko@yahoo.com>
2023-01-08 17:58:16 +0200

    add quick push/pull command

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -3,3 +3,31 @@ vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiff)
 vim.keymap.set("n", "<leader>gl", vim.cmd.Gclog)
 vim.keymap.set("n", "<leader>gb", function() vim.cmd.Git("blame") end)
 
+
+local fugitive_cmd_group = vim.api.nvim_create_augroup("fugitive_cmd_group", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("BufWinEnter", {
+    group = fugitive_cmd_group,
+    pattern = "*",
+    callback = function()
+        if vim.bo.ft ~= "fugitive" then
+            return
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = {buffer = bufnr, remap = false}
+        vim.keymap.set("n", "<leader>gp", function()
+            vim.cmd.Git('push')
+        end, opts)
+
+        -- rebase always
+        vim.keymap.set("n", "<leader>gP", function()
+            vim.cmd.Git('pull --rebase')
+        end, opts)
+
+        -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+        -- needed if i did not set the branch up correctly
+        vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+    end,
+})
