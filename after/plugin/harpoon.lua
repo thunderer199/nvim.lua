965ae86ea778512ab67163196325fead614358a5
Vlad Lashko <v.lashko@yahoo.com>
2023-01-08 18:10:55 +0200

    refactor harpoon config

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,5 +1,6 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
@@ -8,7 +9,7 @@ vim.keymap.set("n", "<leader>aa", mark.rm_file)
 vim.keymap.set("n", "<C-n>", function() ui.nav_next() end)
 vim.keymap.set("n", "<C-z>", function() ui.nav_prev() end)
 
-require('harpoon').setup({
+ harpoon.setup({
 	global_settings = {
 		mark_branch = true
 	}
