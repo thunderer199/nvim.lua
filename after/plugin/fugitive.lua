a188eddca59a4dd7f7e32aa89d9d4560db9e98c2
Vlad Lashko <v.lashko@yahoo.com>
2023-01-08 17:53:56 +0200

    remove fugitive grep

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -3,8 +3,3 @@ vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiff)
 vim.keymap.set("n", "<leader>gl", vim.cmd.Gclog)
 vim.keymap.set("n", "<leader>gb", function() vim.cmd.Git("blame") end)
 
-
--- fugitive live grep
-vim.keymap.set("n", "<leader>gg", function()
-  vim.cmd.Git("grep -n --no-color -I -i -e " .. vim.fn.input("Grep for > "))
-end)
