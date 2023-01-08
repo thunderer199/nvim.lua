90dbe8527dce51fb63e081b37edcc36455c00502
Vlad Lashko <v.lashko@yahoo.com>
2023-01-08 17:49:40 +0200

    cmp expand mapping

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -25,6 +25,9 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-e>'] = cmp.mapping.close(),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ["<C-/>"] = cmp.mapping.complete(),
 })
