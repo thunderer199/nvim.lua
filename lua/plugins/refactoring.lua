7d4bdf7863afdb53005e9c11f267931dfd21a9b3
Vlad Lashko <v.lashko@yahoo.com>
2023-02-14 21:58:36 +0200

    improve refactoring config

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -7,7 +7,16 @@ return {
     config = function()
         local refactoring = require('refactoring')
 
-        vim.keymap.set("n", "<leader>rr", refactoring.select_refactor)
+        vim.api.nvim_set_keymap("v", "<leader>re",
+            [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR>]],
+            { noremap = true, silent = true, expr = false })
+        vim.api.nvim_set_keymap("v", "<leader>rv",
+            [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]],
+            { noremap = true, silent = true, expr = false })
+        vim.api.nvim_set_keymap("v", "<leader>ri",
+            [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]],
+            { noremap = true, silent = true, expr = false })
+
         vim.keymap.set("v", "<leader>rr", refactoring.select_refactor)
     end
 }
