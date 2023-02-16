7f645caf3b5e068eedb0c0a08689a860348d630e
Vlad Lashko <v.lashko@yahoo.com>
2023-02-16 21:04:08 +0200

    lualine show path

--- lua/plugins/lua-line.lua
+++ lua/plugins/lua-line.lua
@@ -8,7 +8,23 @@ return {
             sections = {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch', 'diff' },
-                lualine_c = { 'diagnostics', 'filename' },
+                lualine_c = {
+                    'diagnostics',
+                    {
+                        'filename',
+                        file_status = true,
+                        newfile_status = true,
+                        path = 1,
+                        shorting_target = 40,
+                        symbols = {
+                            modified = '[+]', -- Text to show when the file is modified.
+                            readonly = '[r/o]', -- Text to show when the file is non-modifiable or readonly.
+                            unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                            newfile = '[New]', -- Text to show for newly created file before first write
+                            git = '[Git]', -- Text to show when the file is modified.
+                        }
+                    }
+                },
                 lualine_x = { 'encoding', 'fileformat', 'filetype' },
                 lualine_y = { 'progress', 'searchcount' },
                 lualine_z = { 'location' }
