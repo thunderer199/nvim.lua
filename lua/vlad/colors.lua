8c7737fb933795299d1eb5ca3367ef10f56dd587
Vlad Lashko <v.lashko@yahoo.com>
2023-02-17 22:14:51 +0200

    add transparency via autocommand

--- lua/vlad/colors.lua
+++ lua/vlad/colors.lua
@@ -1,9 +1,1 @@
-function ColorMe(color)
-  color = color or "duskfox"
-  vim.cmd.colorscheme(color)
-
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-ColorMe()
+vim.cmd.colorscheme("duskfox")
