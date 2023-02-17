82b39d9544cce5fc39d35f100d4b0a0efdf6bb91
Vlad Lashko <v.lashko@yahoo.com>
2023-02-17 22:15:17 +0200

    new refactoring map

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -4,19 +4,16 @@ return {
         { "nvim-lua/plenary.nvim" },
         { "nvim-treesitter/nvim-treesitter" }
     },
-    config = function()
-        local refactoring = require('refactoring')
-
-        vim.api.nvim_set_keymap("v", "<leader>re",
-            [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR>]],
-            { noremap = true, silent = true, expr = false })
-        vim.api.nvim_set_keymap("v", "<leader>rv",
-            [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]],
-            { noremap = true, silent = true, expr = false })
-        vim.api.nvim_set_keymap("v", "<leader>ri",
-            [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]],
-            { noremap = true, silent = true, expr = false })
-
-        vim.keymap.set({"n", "v"}, "<leader>rr", refactoring.select_refactor)
-    end
+    keys = {
+      {
+        "<leader>r",
+        function()
+          require("refactoring").select_refactor()
+        end,
+        mode = { "n", "v" },
+        noremap = true,
+        silent = true,
+        expr = false,
+      },
+    },
 }
