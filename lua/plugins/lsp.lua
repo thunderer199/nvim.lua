4d786a89f5bdf7e22024b10114a95e258235917f
Vlad Lashko <v.lashko@yahoo.com>
2023-02-19 14:56:52 +0200

    lsp mapping change for productivity

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -75,9 +75,9 @@ return {
             vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
             vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
             vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-            vim.keymap.set({ "n", "v" }, "<leader>vca", vim.lsp.buf.code_action, opts)
-            vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-            vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+            vim.keymap.set({ "n", "v" }, "<leader>va", vim.lsp.buf.code_action, opts)
+            vim.keymap.set("n", "<leader>vr", vim.lsp.buf.references, opts)
+            vim.keymap.set("n", "<leader>vn", vim.lsp.buf.rename, opts)
             vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 
             -- experimental
