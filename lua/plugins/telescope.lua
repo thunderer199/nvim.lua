7ac387f84940b07d4eda84c77a1123150fab8161
Vlad Lashko <v.lashko@yahoo.com>
2023-03-26 11:35:28 +0300

    add plugin for live grep for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,15 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.0',
-    dependencies = { { 'nvim-lua/plenary.nvim' } },
+    dependencies = { { 'nvim-lua/plenary.nvim' }, { "nvim-telescope/telescope-live-grep-args.nvim" } },
     config = function()
+        local telescope = require('telescope')
+        local lga_actions = require("telescope-live-grep-args.actions")
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+        telescope.load_extension("live_grep_args")
+
+        vim.keymap.set('n', '<leader>fg', telescope.extensions.live_grep_args.live_grep_args, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
