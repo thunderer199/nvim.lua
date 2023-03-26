02bf048a814dc49ccc0beb031f2fe4838e5549ca
Vlad Lashko <v.lashko@yahoo.com>
2023-03-26 14:43:44 +0300

    map harpoon quick jump

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -7,10 +7,16 @@ return {
 
         vim.keymap.set("n", "<leader>a", mark.add_file)
         vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-        vim.keymap.set("n", "<leader>aa", mark.rm_file)
 
         vim.keymap.set("n", "<C-n>", function() ui.nav_next() end)
         vim.keymap.set("n", "<C-z>", function() ui.nav_prev() end)
+
+
+        -- map alt + hjkl to navigate between files
+        vim.keymap.set("n", "˙", function() ui.nav_file(1) end)
+        vim.keymap.set("n", "∆", function() ui.nav_file(2) end)
+        vim.keymap.set("n", "˚", function() ui.nav_file(3) end)
+        vim.keymap.set("n", "¬", function() ui.nav_file(4) end)
 
         harpoon.setup({
             global_settings = {
