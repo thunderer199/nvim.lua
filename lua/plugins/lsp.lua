10b282d609c801c60b66a28f7ade55a537e807c2
Vlad Lashko <v.lashko@yahoo.com>
2023-04-16 05:48:24 +0300

    more convenient lsp keybindings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -72,20 +72,20 @@ return {
                 vim.lsp.buf.execute_command({ command = "_typescript.organizeImports", arguments = { vim.fn.expand("%:p") } })
             end)
 
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
             vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
             vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
             vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
             vim.keymap.set({ "n", "v" }, "<leader>va", vim.lsp.buf.code_action, opts)
-            vim.keymap.set("n", "<leader>vr", vim.lsp.buf.references, opts)
             vim.keymap.set("n", "<leader>vn", vim.lsp.buf.rename, opts)
             vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+            vim.keymap.set("n", "<leader>fl", vim.diagnostic.open_float, opts)
 
             -- experimental
-            vim.keymap.set("n", "<leader>fl", vim.diagnostic.open_float, opts)
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
             vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-            vim.keymap.set("n", "<leader>i", vim.lsp.buf.implementation, opts)
         end)
 
         lsp.setup()
