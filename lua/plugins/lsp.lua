b64d2ffa519e6b47e1ca211029c3ab5c96919dd6
Vlad Lashko <v.lashko@yahoo.com>
2023-05-05 19:48:38 +0300

    autocomplete - replace mode

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -28,7 +28,7 @@ return {
         })
 
         local cmp = require('cmp')
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        local cmp_select = { behavior = cmp.SelectBehavior.Replace }
         local cmp_mappings = lsp.defaults.cmp_mappings({
             ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
             ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
