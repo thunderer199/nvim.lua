47b04723b3f846692f19c4b3f469d6aa8fe55c52
Vlad Lashko <v.lashko@yahoo.com>
2023-05-05 19:48:15 +0300

    go to type mapping

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -76,6 +76,8 @@ return {
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
             vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
             vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+            -- go to type definition
+            vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
             vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
             vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
             vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
