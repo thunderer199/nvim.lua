940918dae8cc2296eae4dc2216606a4c187202d5
Vlad Lashko <v.lashko@yahoo.com>
2023-10-29 11:52:51 +0200

    new mapping for Oil

--- lua/vlad/remap.lua
+++ lua/vlad/remap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pv", ':Oil<CR>')
+vim.keymap.set("n", "-", ":Oil<CR>")
 
 -- move undent
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
