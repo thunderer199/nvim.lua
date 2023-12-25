81b67cf760b61aa2d0c8ed1d841ce5a29d0ee6eb
Vlad Lashko <v.lashko@yahoo.com>
2023-12-25 11:04:42 +0200

    quickfix toggle map

---
+++ lua/vlad/quickfix.lua
@@ -0,0 +1,23 @@
+local function toggle_quickfix()
+    local windows = vim.api.nvim_list_wins()
+    local quickfix_open = false
+
+    for _, window in pairs(windows) do
+        if vim.api.nvim_win_get_config(window).relative == '' then
+            local buf = vim.api.nvim_win_get_buf(window)
+            if vim.bo[buf].buftype == 'quickfix' then
+                quickfix_open = true
+                break
+            end
+        end
+    end
+
+    if quickfix_open then
+        vim.cmd('cclose')
+    else
+        vim.cmd('copen')
+    end
+end
+
+
+vim.keymap.set("n", "<leader>qt", toggle_quickfix)
