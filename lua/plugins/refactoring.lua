177baab0e9181b4e8a46a41860a0a136d9ad9c11
Vlad Lashko <v.lashko@yahoo.com>
2024-01-05 14:21:29 +0200

    add mapping for refactoring plugin

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -1,19 +1,14 @@
 return {
-    "ThePrimeagen/refactoring.nvim",
-    dependencies = {
-        { "nvim-lua/plenary.nvim" },
-        { "nvim-treesitter/nvim-treesitter" }
-    },
-    keys = {
-      {
-        "<leader>r",
-        function()
-          require("refactoring").select_refactor()
-        end,
-        mode = { "n", "v" },
-        noremap = true,
-        silent = true,
-        expr = false,
-      },
-    },
+  "ThePrimeagen/refactoring.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-treesitter/nvim-treesitter" }
+  },
+  keys = {
+    { "<leader>r", function() require("refactoring").select_refactor() end, mode = { "n", "v" }, noremap = true, silent = true, expr = false },
+    { "<leader>rf", function() require("refactoring").refactor("Extract Function") end, mode = { "n", "x", "v" }, noremap = true, silent = true, expr = false },
+    { "<leader>rv", function() require("refactoring").refactor("Extract Variable") end, mode = { "n", "x", "v" }, noremap = true, silent = true, expr = false },
+    { "<leader>rI", function() require("refactoring").refactor("Inline Function") end, mode = { "n", "x", "v" }, noremap = true, silent = true, expr = false },
+    { "<leader>ri", function() require("refactoring").refactor("Inline Variable") end, mode = { "n", "x", "v" }, noremap = true, silent = true, expr = false },
+  },
 }
