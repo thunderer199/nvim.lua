f29451952c79a5cbf89f9b0e5a8924fc478ec10d
Vlad Lashko <v.lashko@yahoo.com>
2024-01-12 21:43:02 +0200

    better mapping with Lazy

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,10 +16,12 @@ return {
     {
         'goolord/alpha-nvim',
         dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+        lazy = false,
+        keys = {
+            { "<leader>al", '<cmd>Alpha<cr>' }
+        },
         config = function()
             require 'alpha'.setup(require 'alpha.themes.startify'.config)
-
-            vim.keymap.set('n', '<leader>al', '<cmd>Alpha<cr>')
         end
     },
     { 'mbbill/undotree', keys = { { "<leader>u", vim.cmd.UndotreeToggle } } },
