de52ba9b1c167ceb15d8c778efa0473e0005ac5d
Vlad Lashko <v.lashko@yahoo.com>
2024-01-12 21:43:42 +0200

    better name

--- lua/vlad/commands.lua
+++ lua/vlad/commands.lua
@@ -1,7 +1,7 @@
-vim.api.nvim_create_user_command("Cppath", function()
+vim.api.nvim_create_user_command("CopyPath", function()
     local path = vim.fn.expand("%")
     vim.fn.setreg("+", path)
     vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, {})
 
-vim.keymap.set('n', '<leader>cp', ':Cppath<CR>')
+vim.keymap.set('n', '<leader>cp', ':CopyPath<CR>')
