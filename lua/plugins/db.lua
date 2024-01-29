1978365a90b89f106f5644a585b3b7e89450a577
Vlad Lashko <v.lashko@yahoo.com>
2024-01-29 09:33:39 +0200

    db plugin

---
+++ lua/plugins/db.lua
@@ -0,0 +1,28 @@
+return {
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod',                     lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+  },
+  cmd = { 'DBUI', 'DBUIToggle', 'DBUIAddConnection', 'DBUIFindBuffer' },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_execute_on_save = 0
+
+    vim.keymap.set('n', '<leader>du', '<cmd>DBUIToggle<CR>');
+
+    vim.keymap.set(
+      'n',
+      '<leader>dr',
+      ':normal vip<CR><PLUG>(DBUI_ExecuteQuery)'
+    );
+
+    vim.keymap.set(
+      'n',
+      '<leader>ds',
+      '<PLUG>(DBUI_ExecuteQuery)'
+    );
+
+  end,
+}
