35b19f2cfd9f25a600f227326e5af08ef5ab6c48
Vlad Lashko <v.lashko@yahoo.com>
2024-02-04 01:40:54 +0200

    telescope for git history

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
+    -- tag = '0.1.5',
     dependencies = {
         'nvim-lua/plenary.nvim',
         "nvim-telescope/telescope-live-grep-args.nvim",
@@ -34,11 +34,15 @@ return {
         telescope.load_extension("live_grep_args")
 
         vim.keymap.set('n', '<leader>fg', telescope.extensions.live_grep_args.live_grep_args, {})
-        vim.keymap.set('n', '<leader>fgh', live_grep_args_shortcuts.grep_word_under_cursor, {})
+        vim.keymap.set('n', '<leader>fh', live_grep_args_shortcuts.grep_word_under_cursor, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>fhh', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
         vim.keymap.set('n', '<leader>fr', builtin.resume, {})
+
+        vim.keymap.set('n', '<leader>fc', builtin.git_commits, {})
+        vim.keymap.set('n', '<leader>fC', builtin.git_bcommits, {})
+        vim.keymap.set('v', '<leader>fC', builtin.git_bcommits_range, {})
 
         vim.keymap.set('n', '<leader>fo', function()
             builtin.oldfiles({ cwd_only = true, shorten_path = true, })
@@ -50,7 +54,7 @@ return {
 
         vim.keymap.set('n', '<leader>ft', builtin.treesitter)
         vim.keymap.set('n', '<leader>ftt', ':TodoTelescope<CR>')
-        vim.keymap.set('n', '<leader>fc', builtin.colorscheme)
+        -- vim.keymap.set('n', '<leader>fc', builtin.colorscheme)
 
         local git_files_fallback_to_find_file = function()
             -- define here if you want to define something
