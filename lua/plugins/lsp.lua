c28bcbef25b93d21c7beca02220c8a4761e99e91
Vlad Lashko <v.lashko@yahoo.com>
2024-02-09 17:55:58 +0000

    Enter replace suggestions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -37,7 +37,8 @@ return {
             ['<C-d>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-e>'] = cmp.mapping.close(),
-            ['<C-y>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+            ['<C-l>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace }),
+            ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace }),
             ["<C-s>"] = cmp.mapping(cmp.mapping.complete(), { "i", "s" }),
         })
 
