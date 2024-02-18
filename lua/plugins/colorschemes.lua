fd87ff1febd53d8257863803ee465885dc114f87
Vlad Lashko <v.lashko@yahoo.com>
2024-02-18 12:18:05 +0000

    new default theme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,10 +2,11 @@ return {
     'EdenEast/nightfox.nvim',
     'ellisonleao/gruvbox.nvim',
     'folke/tokyonight.nvim',
+    { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
     {
         'xiyaowong/transparent.nvim',
         lazy = false,
-        config = function ()
+        config = function()
             require('transparent').clear_prefix('lualine')
         end
     }
