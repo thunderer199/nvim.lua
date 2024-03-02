7cd4fcd4ff6ae3aa8b72adb06d61e71696039650
Vlad Lashko <v.lashko@yahoo.com>
2024-03-02 08:35:13 +0000

    zen mode and theme adjustments

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,12 +2,34 @@ return {
     'EdenEast/nightfox.nvim',
     'ellisonleao/gruvbox.nvim',
     'folke/tokyonight.nvim',
-    { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+    {
+        'catppuccin/nvim',
+        name = 'catppuccin',
+        priority = 1000,
+        opts = {
+
+            custom_highlights = {
+                LineNr = { fg = '#6e7891' },
+            },
+            integrations = {
+                harpoon = true
+            }
+        }
+    },
     {
         'xiyaowong/transparent.nvim',
         lazy = false,
         config = function()
             require('transparent').clear_prefix('lualine')
         end
+    },
+    -- Lua
+    {
+        "folke/zen-mode.nvim",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
     }
 }
