75f347998cdf41826ca57d45d2acbe15121a86b7
Vlad Lashko <v.lashko@yahoo.com>
2024-03-21 14:53:34 +0000

    remove unused mapping

--- lua/vlad/remap.lua
+++ lua/vlad/remap.lua
@@ -11,10 +11,6 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
--- delete to void buffer
--- vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
--- vim.keymap.set({ "n", "v" }, "<leader>D", [["_D]])
-
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set('n', 'QQ', '<cmd>q!<CR>')
 
