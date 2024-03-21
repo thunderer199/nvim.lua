88110d6c2636f8a7c51b21704e6291e6cbca57ac
Vlad Lashko <v.lashko@yahoo.com>
2024-03-21 14:30:46 +0000

    remove move selection mapping

--- lua/vlad/remap.lua
+++ lua/vlad/remap.lua
@@ -1,10 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "-", ":Oil<CR>")
 
--- move undent
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
 -- stay in center
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
