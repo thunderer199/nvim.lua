aad32897546d86539a74fd5de1b9b7e08997be92
Vlad Lashko <v.lashko@yahoo.com>
2024-03-30 20:29:30 +0000

    refactor(git): remove redundant git diff commands
    use diffviewopen instead

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -120,10 +120,9 @@ return {
                     map('n', '<leader>hu', gs.undo_stage_hunk)
                     map('n', '<leader>hR', gs.reset_buffer)
                     map('n', '<leader>hp', gs.preview_hunk)
+
                     map('n', '<leader>hb', function() gs.blame_line { full = true } end)
                     map('n', '<leader>tb', gs.toggle_current_line_blame)
-                    map('n', '<leader>hd', gs.diffthis)
-                    map('n', '<leader>hD', function() gs.diffthis('~') end)
                     map('n', '<leader>td', gs.toggle_deleted)
 
                     -- Text object
