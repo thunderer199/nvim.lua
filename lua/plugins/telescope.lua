f9adfb1b1c3aa931689c505531635891d0c7e102
Vlad Lashko <v.lashko@yahoo.com>
2024-03-31 09:41:27 +0000

    Add 'telescope-dap.nvim' extension to Telescope in Neovim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
         'nvim-lua/plenary.nvim',
         "nvim-telescope/telescope-live-grep-args.nvim",
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        'nvim-telescope/telescope-dap.nvim',
     },
     config = function()
         local telescope = require('telescope')
@@ -141,6 +142,7 @@ return {
         }
         telescope.load_extension("live_grep_args")
         telescope.load_extension("fzf")
+        telescope.load_extension("dap")
 
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fB', builtin.git_branches, {})
