b7717dfc8868c7663ce9174bf04f7e20f82b5bb7
Vlad Lashko <v.lashko@yahoo.com>
2024-06-23 08:14:51 +0000

    Add utility functions to find parent directory with package.json and check string prefix

--- lua/vlad/util.lua
+++ lua/vlad/util.lua
@@ -40,7 +40,36 @@ local function trim_quotes(s)
     return s
 end
 
+local function starts_with(full_string, prefix)
+    return full_string:sub(1, #prefix) == prefix
+end
 
+local find_parent_with_package_json = function(path)
+    if path == "" then
+        return nil
+    end
+    if path == nil then
+        return nil
+    end
+    -- start with / to avoid infinite loop
+    if not starts_with(path, "/") then
+        return nil
+    end
+
+    local current_path = path
+    while current_path ~= "/" do
+        local package_json = current_path .. "/package.json"
+        if vim.fn.filereadable(package_json) == 1 then
+            return current_path
+        end
+        current_path = vim.fn.fnamemodify(current_path, ":h")
+    end
+    return nil
+end
+
+
+M.find_parent_with_package_json = find_parent_with_package_json;
+M.starts_with = starts_with;
 M.get_git_cwd = get_git_cwd;
 M.get_base_path = get_base_path;
 M.split_into_lines = split_into_lines;
