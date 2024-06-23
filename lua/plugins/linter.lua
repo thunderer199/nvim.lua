2157d654c4f187d074e40a58e05f48300bb07850
Vlad Lashko <v.lashko@yahoo.com>
2024-06-23 08:21:45 +0000

    Linter: pass cwd to linter command

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -15,12 +15,18 @@ return {
       stylus = { "stylelint" },
     }
 
+    local util = require("vlad.util")
+
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
-        lint.try_lint(nil, { ignore_errors = true })
+        local dir = util.find_parent_with_package_json(vim.fn.expand("%:p"))
+        lint.try_lint(nil, {
+          ignore_errors = true,
+          cwd = dir,
+        })
       end,
     })
   end,
