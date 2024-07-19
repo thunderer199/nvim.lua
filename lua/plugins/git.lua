e2904c28172d31a3be421cbe17d868fbc17a8996
Vlad Lashko <v.lashko@yahoo.com>
2024-07-19 07:52:16 +0000

    fix gitsigns package

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -91,6 +91,7 @@ return {
     },
     {
         'lewis6991/gitsigns.nvim',
+        tag = 'v0.9.0',
         config = function()
             require('gitsigns').setup {
                 on_attach = function(bufnr)
