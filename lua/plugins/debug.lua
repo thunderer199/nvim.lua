9dca0e9f4f8786bbd45bbb6bfc1d1c8408ead78e
Vlad Lashko <v.lashko@yahoo.com>
2024-07-28 17:01:12 +0000

    Refactor debug UI layout and add logpoint functionality

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -27,16 +27,16 @@ return {
                             elements = {
                                 { id = "stacks",      size = 0.25 },
                                 { id = "scopes",      size = 0.5 },
-                                { id = "breakpoints", size = 0.25 },
+                                { id = "watches", size = 0.25 },
                             },
                             position = "left",
                             size = 0.25
                         },
-                        {
-                            elements = { id = "watches", size = 0.25 },
-                            position = "bottom",
-                            size = 10,
-                        }
+                        -- {
+                        --     elements = { id = "watches", size = 0.25 },
+                        --     position = "bottom",
+                        --     size = 10,
+                        -- }
                     },
                     mappings = {
                         edit = "e",
@@ -154,19 +154,24 @@ return {
             vim.keymap.set('n', '<leader>db', function() breakpoints.toggle_breakpoint() end,
                 { desc = "Debug - Toggle breakpoint" })
 
+            vim.keymap.set('n', '<leader>dn', function()
+                breakpoints.set_log_point()
+            end, { desc = "Debug - Logpoint" })
+            -- vim.keymap.set('n', '<Leader>dn', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+
+
             vim.keymap.set('n', '<leader>dB', function()
                 -- dap.set_breakpoint(vim.fn.input('Breakpoint condition: ))
                 breakpoints.set_conditional_breakpoint(vim.fn.input('Breakpoint condition: '))
-            end, {
-                desc = "Debug - Set breakpoint with condition"
-            })
+            end, { desc = "Debug - Set breakpoint with condition" })
 
             vim.keymap.set('n', '<leader>dq', function() dap.step_out() end, { desc = "Debug - Step out" })
             vim.keymap.set('n', '<leader>dw', function() dap.step_over() end, { desc = "Debug - Step over" })
             vim.keymap.set('n', '<leader>de', function() dap.step_into() end, { desc = "Debug - Step into" })
 
             vim.keymap.set('n', '<leader>di', function() dapui.eval() end, { desc = "Debug - Evaluate" })
-            vim.keymap.set('n', '<leader>dW', function() dapui.float_element("watches") end, { desc = "Debug - Watches" })
+            vim.keymap.set('n', '<leader>dl', function() dapui.float_element("breakpoints") end, { desc = "Debug - breakpoints list" })
+            vim.keymap.set('n', '<leader>dL', function() breakpoints.clear_all_breakpoints() end, { desc = "Debug - Clear all breakpoints" })
 
             vim.keymap.set('n', '<leader>do', function() dapui.toggle() end, { desc = "Debug - Toggle UI" })
 
