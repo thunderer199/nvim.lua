944804d967de3ad3c58bc291e2e4a09143d18a81
Vlad Lashko <v.lashko@yahoo.com>
2024-08-26 10:57:45 +0000

    Add function to find parent directory containing a specified file

--- lua/vlad/util.lua
+++ lua/vlad/util.lua
@@ -44,6 +44,32 @@ local function starts_with(full_string, prefix)
     return full_string:sub(1, #prefix) == prefix
 end
 
+-- Find the parent directory that contains a file
+---@param path string 
+---@param file_name string
+local find_parent_with_file = function(path, file_name)
+    if path == "" then
+        return nil
+    end
+    if path == nil then
+        return nil
+    end
+    -- start with / to avoid infinite loop
+    if not starts_with(path, "/") then
+        return nil
+    end
+
+    local current_path = path
+    while current_path ~= "/" do
+        local package_json = current_path .. "/" .. file_name
+        if vim.fn.filereadable(package_json) == 1 then
+            return current_path
+        end
+        current_path = vim.fn.fnamemodify(current_path, ":h")
+    end
+    return nil
+end
+
 local find_parent_with_package_json = function(path)
     if path == "" then
         return nil
@@ -69,6 +95,7 @@ end
 
 
 M.find_parent_with_package_json = find_parent_with_package_json;
+M.find_parent_with_file = find_parent_with_file;
 M.starts_with = starts_with;
 M.get_git_cwd = get_git_cwd;
 M.get_base_path = get_base_path;
