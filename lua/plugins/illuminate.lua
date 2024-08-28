39c387356bcb5f7fcfc2ee57e5a33e512500eebe
Vlad Lashko <v.lashko@yahoo.com>
2024-08-28 10:55:21 +0000

    configure illuminate

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -1,11 +1,11 @@
 return {
     'RRethy/vim-illuminate',
     event = "VeryLazy",
-    opts = {
-        delay = 200,
-        large_file_cutoff = 2000,
-        large_file_overrides = {
-            -- providers: provider used to get references in the buffer, ordered by priority
+    config = function()
+        local illuminate = require('illuminate');
+
+        illuminate.configure({
+            delay = 200,
             providers = {
                 'lsp',
                 'treesitter',
@@ -17,18 +17,19 @@ return {
                 'dirvish',
                 'fugitive',
                 'oil',
+                'telescope',
             },
             under_cursor = true,
-            min_count_to_highlight = 1,
-        },
-    },
-    config = function(_, opts)
-        local illuminate = require('illuminate');
+            min_count_to_highlight = 2,
+        })
 
-        illuminate.configure(opts)
+        vim.keymap.set('n', ']]', illuminate.goto_next_reference,
+            { noremap = true, silent = true, desc = 'Go to next reference' })
+        vim.keymap.set('n', '[[', illuminate.goto_prev_reference,
+            { noremap = true, silent = true, desc = 'Go to previous reference' })
 
-        vim.keymap.set('n', ']]', illuminate.goto_next_reference, { noremap = true, silent = true, desc = 'Go to next reference' })
-        vim.keymap.set('n', '[[', illuminate.goto_prev_reference, { noremap = true, silent = true, desc = 'Go to previous reference' })
-
+        vim.api.nvim_set_hl(0, 'IlluminatedWordText', { link = 'Visual' })
+        vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { link = 'Visual' })
+        vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { link = 'Visual' })
     end
 }
