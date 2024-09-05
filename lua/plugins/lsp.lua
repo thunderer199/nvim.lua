d928b6c87af476d5c1bb4b104c3b0f698f995422
Vlad Lashko <v.lashko@yahoo.com>
2024-09-05 06:04:16 +0000

    Configure tsserver with TypeScript inlay hints in Neovim LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -42,6 +42,35 @@ return {
             },
         })
 
+        lsp.configure('tsserver', {
+            settings = {
+                typescript = {
+                    inlayHints = {
+                        includeInlayParameterNameHints = "all",
+                        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                        includeInlayFunctionParameterTypeHints = true,
+                        includeInlayVariableTypeHints = true,
+                        includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+                        includeInlayPropertyDeclarationTypeHints = true,
+                        includeInlayFunctionLikeReturnTypeHints = true,
+                        includeInlayEnumMemberValueHints = true,
+                    },
+                },
+                -- javascript = {
+                --     inlayHints = {
+                --         includeInlayParameterNameHints = "all",
+                --         includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                --         includeInlayFunctionParameterTypeHints = true,
+                --         includeInlayVariableTypeHints = true,
+                --         includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+                --         includeInlayPropertyDeclarationTypeHints = true,
+                --         includeInlayFunctionLikeReturnTypeHints = true,
+                --         includeInlayEnumMemberValueHints = true,
+                --     },
+                -- },
+            }
+        })
+
         lsp.configure('basedpyright', {
             settings = {
                 basedpyright = {
