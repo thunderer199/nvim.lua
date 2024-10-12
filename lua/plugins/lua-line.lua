cb6782843edfd62db801478300d5c1c444c532bc
Vlad Lashko <v.lashko@yahoo.com>
2024-10-12 12:51:17 +0000

    use opts syntax for lua plugins

--- lua/plugins/lua-line.lua
+++ lua/plugins/lua-line.lua
@@ -1,52 +1,50 @@
 return {
     'nvim-lualine/lualine.nvim',
-    config = function()
-        require('lualine').setup {
-            options = {
-                theme = 'auto',
-            },
-            sections = {
-                lualine_a = { 'mode' },
-                lualine_b = { 'branch', 'diff' },
-                lualine_c = {
-                    'diagnostics',
-                    {
-                        'filename',
-                        file_status = true,
-                        newfile_status = true,
-                        path = 1,
-                        shorting_target = 40,
-                        symbols = {
-                            modified = '[+]',      -- Text to show when the file is modified.
-                            readonly = '[r/o]',    -- Text to show when the file is non-modifiable or readonly.
-                            unnamed = '[No Name]', -- Text to show for unnamed buffers.
-                            newfile = '[New]',     -- Text to show for newly created file before first write
-                            git = '[Git]',         -- Text to show when the file is modified.
-                        }
+    opts = {
+        options = {
+            theme = 'auto',
+        },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch', 'diff' },
+            lualine_c = {
+                'diagnostics',
+                {
+                    'filename',
+                    file_status = true,
+                    newfile_status = true,
+                    path = 1,
+                    shorting_target = 40,
+                    symbols = {
+                        modified = '[+]',          -- Text to show when the file is modified.
+                        readonly = '[r/o]',        -- Text to show when the file is non-modifiable or readonly.
+                        unnamed = '[No Name]',     -- Text to show for unnamed buffers.
+                        newfile = '[New]',         -- Text to show for newly created file before first write
+                        git = '[Git]',             -- Text to show when the file is modified.
                     }
-                },
-                lualine_x = {
-                    -- {
-                    --     require("noice").api.statusline.mode.get,
-                    --     cond = require("noice").api.statusline.mode.has,
-                    --     color = { fg = "#ff9e64" },
-                    -- },
-                    'encoding',
-                    'fileformat',
-                    'filetype',
-                },
-                lualine_y = { 'progress', 'searchcount' },
-                lualine_z = { 'location' }
+                }
             },
-            inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = { 'filename' },
-                lualine_x = { 'location' },
-                lualine_y = {},
-                lualine_z = {}
+            lualine_x = {
+                -- {
+                --     require("noice").api.statusline.mode.get,
+                --     cond = require("noice").api.statusline.mode.has,
+                --     color = { fg = "#ff9e64" },
+                -- },
+                'encoding',
+                'fileformat',
+                'filetype',
             },
-            tabline = {},
-        }
-    end
+            lualine_y = { 'progress', 'searchcount' },
+            lualine_z = { 'location' }
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { 'filename' },
+            lualine_x = { 'location' },
+            lualine_y = {},
+            lualine_z = {}
+        },
+        tabline = {},
+    },
 }
--- lua/plugins/tree-sj.lua
+++ lua/plugins/tree-sj.lua
@@ -4,7 +4,5 @@ return {
     { '<leader>m', function() require('treesj').toggle() end, desc = 'Toggle Treesitter Join' },
   },
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  config = function()
-    require('treesj').setup({ max_join_length = 1000, use_default_keymaps = false })
-  end,
+  opts = { max_join_length = 1000, use_default_keymaps = false },
 }
