e954e8812e61c9630329cf17eff8f9f664eae079
Vlad Lashko <v.lashko@yahoo.com>
2024-10-21 06:59:33 +0000

    replace plugins for close buffers with a custom command

--- lua/plugins/buffers.lua
+++
@@ -1,6 +0,0 @@
-return {
-    {
-        'Asheq/close-buffers.vim',
-        cmd = { 'Bdelete' },
-    }
-}
--- lua/vlad/commands.lua
+++ lua/vlad/commands.lua
@@ -6,3 +6,12 @@ end, {})
 
 vim.keymap.set('n', '<leader>cp', ':CopyPath<CR>')
 
+vim.api.nvim_create_user_command("BuffersClose", function()
+	local curr_buf = vim.api.nvim_get_current_buf()
+	local bufs = vim.api.nvim_list_bufs()
+	for _, buf in ipairs(bufs) do
+		if buf ~= curr_buf then
+			vim.api.nvim_buf_delete(buf, {})
+		end
+	end
+end, {})
