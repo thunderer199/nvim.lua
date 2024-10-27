424cf7525ca47828c24db99fb6c775655b988d38
Vlad Lashko <v.lashko@yahoo.com>
2024-10-27 07:20:16 +0000

    improve react useState snippet

--- lua/vlad/snippets/typescriptreact.lua
+++ lua/vlad/snippets/typescriptreact.lua
@@ -1,10 +1,12 @@
 local ls = require("luasnip")
 local fmts = require("vlad.util").snippet_fmts
+local fmt = require('luasnip.extras.fmt').fmt
 
 local ts = require("vlad.snippets.typescript")
 
 local i = ls.insert_node
 local s = ls.snippet
+local f = ls.function_node
 
 
 local reactFuncComp = fmts(
@@ -63,9 +65,28 @@ export const [ComponentName]: React.FC<[ComponentName]Props> = (props) => {
   }
 );
 
+local reactUseState = fmt(
+  [[
+    const [{getVal}, set{SetVal}] = useState({initialVal});
+    ]],
+  {
+    getVal = i(1, "getVal"),
+    initialVal = i(2, "initialVal"),
+    SetVal = f(function(params)
+      local args = params[1]
+      return args[1]:sub(1, 1):upper() .. args[1]:sub(2)
+    end, { 1 })
+  },
+  {
+    repeat_duplicates = true
+  }
+);
+
+
 local snippets = {
   s('rfc', reactFuncComp),
   s('rfcp', reactFuncCompProp),
+  s('us', reactUseState),
 }
 
 return vim.list_extend(ts, snippets)
