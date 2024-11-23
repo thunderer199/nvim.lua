cedb936bb9867f4fce565a451c158751f5cbd28d
Vlad Lashko <v.lashko@yahoo.com>
2024-11-23 13:13:20 +0000

    feat: add snacks.nvim plugin with buffer management

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,28 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        bigfile = {
+            enabled = true,
+            notify = true,
+        },
+        -- notifier = { enabled = true },
+        quickfile = { enabled = true },
+        -- statuscolumn = { enabled = true },
+        -- words = { enabled = true },
+    },
+    keys = {
+        {
+            "<leader>bd",
+            function()
+                ---@class snacks
+                Snacks.bufdelete.other()
+            end,
+            desc = "Delete Buffer"
+        },
+    }
+}
