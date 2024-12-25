bd7abc062ee11fbf2aa0e4eeb236404d41a80e70
Vlad Lashko <v.lashko@yahoo.com>
2024-12-25 13:06:15 +0000

    use zen mode from snacks

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -79,13 +79,4 @@ return {
             transparent.clear_prefix('trouble')
         end
     },
-    -- Lua
-    {
-        "folke/zen-mode.nvim",
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-    }
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,49 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
+    init = function()
+        local snacks = require("snacks")
+        vim.api.nvim_create_user_command("ZenMode", function()
+            snacks.zen.zen({ toggles = { dim = false, git_signs = true } })
+        end, {})
+        vim.api.nvim_create_user_command("ZenModeDim", function()
+            snacks.zen.zen({ toggles = { dim = true, git_signs = true } })
+        end, {})
+
+        vim.api.nvim_create_user_command("GitBrowse", function()
+            local util = require('vlad.util')
+            local urls = {
+                github = {
+                    branch = "/tree/{branch}",
+                    file = "/blob/{branch}/{file}#L{line_start}-L{line_end}",
+                    commit = "/commit/{commit}",
+                },
+                gitlab = {
+                    branch = "/-/tree/{branch}",
+                    file = "/-/blob/{branch}/{file}#L{line_start}-L{line_end}",
+                    commit = "/-/commit/{commit}",
+                },
+            }
+
+            local list = {}
+            for service, patterns in pairs(urls) do
+                local file = util.read_file(os.getenv('HOME') .. '/.' .. service .. '-enterprise')
+                if file then
+                    local lines = util.split_into_lines(file)
+                    for _, line in ipairs(lines) do
+                        local domain = line:gsub("%.", "%%.")
+                        domain = domain:gsub("^https?://", "")
+
+                        list[domain] = patterns
+                    end
+                end
+            end
+
+            snacks.gitbrowse.open({
+                url_patterns = list,
+            })
+        end, {})
+    end,
     opts = {
         -- your configuration comes here
         -- or leave it empty to use the default settings
@@ -14,6 +57,18 @@ return {
         quickfile = { enabled = true },
         -- statuscolumn = { enabled = true },
         -- words = { enabled = true },
+        zen = {
+            toggles = {
+                dim = true,
+                git_signs = false,
+                mini_diff_signs = false,
+                -- diagnostics = false,
+                -- inlay_hints = false,
+            },
+            ---@type snacks.win.Config
+            -- win = { style = "zen" },
+
+        },
     },
     keys = {
         {
