e87ad71952134647ea795a4739baf59118248aff
Vlad Lashko <v.lashko@yahoo.com>
2024-12-26 14:15:57 +0000

    remove eslint from linter plugin use one from mason

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -4,11 +4,6 @@ return {
     local lint = require("lint")
 
     lint.linters_by_ft = {
-      javascript = { "eslint" },
-      typescript = { "eslint" },
-      javascriptreact = { "eslint" },
-      typescriptreact = { "eslint" },
-      svelte = { "eslint" },
       css = { "stylelint" },
       scss = { "stylelint" },
       less = { "stylelint" },
