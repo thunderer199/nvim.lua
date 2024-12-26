362c276bf6e9e66d4c6c3e82b8816ff0fbe9d18e
Vlad Lashko <v.lashko@yahoo.com>
2024-12-26 14:13:55 +0000

    redo copilot plugin config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -21,17 +21,6 @@ return {
             { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
         },
         keys = {
-            -- Show help actions with telescope
-            {
-                "<leader>cH",
-                function()
-                    print("Show help actions with telescope")
-                    local actions = require("CopilotChat.actions")
-                    require("CopilotChat.integrations.telescope").pick(actions.help_actions())
-                end,
-                desc = "CopilotChat - Help actions",
-            },
-            -- Show prompts actions with telescope
             {
                 "<leader>cg",
                 function()
@@ -64,69 +53,6 @@ return {
                 desc = "CopilotChat - Toggle Float",
             },
             {
-                "<leader>cT",
-                ":CopilotChatTests<CR>",
-                desc = "CopilotChat - Tests",
-                mode = { "n", "v" },
-            },
-            {
-                "<leader>cF",
-                ":CopilotChatFixDiagnostic<CR>",
-                desc = "CopilotChat - Fix Diagnostic",
-                mode = { "n", "v" },
-            },
-            {
-                "<leader>ch",
-                ":CopilotChatFix<CR>",
-                desc = "CopilotChat - Fix",
-                mode = { "n", "v" },
-            },
-            {
-                "<leader>cD",
-                ":CopilotChatDocs<CR>",
-                desc = "CopilotChat - Docs",
-                mode = { "n", "v" },
-            },
-            {
-                "<leader>cE",
-                ":CopilotChatExplain<CR>",
-                desc = "CopilotChat - Explain",
-                mode = { "n", "v" },
-            },
-            {
-                "<leader>cr",
-                function()
-                    local chat = require("CopilotChat")
-                    chat.ask("Review code and provide feedback, following best practices.")
-                end,
-                desc = "CopilotChat - Review",
-                mode = { "n", "v" },
-            },
-            {
-                "<leader>cs",
-                ":CopilotChatReview<CR>",
-                desc = "CopilotChat - Review (github predefined)",
-                mode = { "n", "v" },
-            },
-            {
-                "<leader>cS",
-                function()
-                    local chat = require("CopilotChat")
-                    chat.ask("Spellcheck the provided code and check grammar. Including strings, comments, and identifiers.")
-                end,
-                desc = "CopilotChat - Spellcheck",
-                mode = { "n", "v" },
-            },
-            {
-                "<leader>cR",
-                function()
-                    local chat = require("CopilotChat")
-                    chat.ask("Review provided code following best industry practices. Refactor code using the provided suggestions.")
-                end,
-                desc = "CopilotChat - Review",
-                mode = { "n", "v" },
-            },
-            {
                 "<leader>cq",
                 function()
                     local input = vim.fn.input("Quick Chat: ")
@@ -135,8 +61,36 @@ return {
                     end
                 end,
                 desc = "CopilotChat - Quick chat",
-            }
+            },
+            {
+                "<leader>cx",
+                ":CopilotChatInline<cr>",
+                mode = "x",
+                desc = "CopilotChat - Inline chat",
+            },
         },
+        config = function(_, opts)
+            local chat = require("CopilotChat")
+            chat.setup(opts)
+
+            local select = require("CopilotChat.select")
+            vim.api.nvim_create_user_command("CopilotChatVisual", function(args)
+                chat.ask(args.args, { selection = select.visual })
+            end, { nargs = "*", range = true })
+
+            vim.api.nvim_create_user_command("CopilotChatInline", function(args)
+                chat.ask(args.args, {
+                    selection = select.visual,
+                    window = {
+                        layout = "float",
+                        relative = "cursor",
+                        width = 1,
+                        height = 0.4,
+                        row = 1,
+                    },
+                })
+            end, { nargs = "*", range = true })
+        end,
         ---@type CopilotChat.config
         opts = {
             model = 'claude-3.5-sonnet',
@@ -144,6 +98,58 @@ return {
                 local select = require("CopilotChat.select")
                 return select.visual(source) or select.buffer(source)
             end,
+            prompts = {
+                BestPracticeFeedback = {
+                    model = 'claude-3.5-sonnet',
+                    prompt =
+                    "Review the provided code thoroughly and offer feedback following best practices. Focus on clarity, correctness, maintainability, and performance.",
+                    system_prompt =
+                    "You are a highly skilled software engineer and code reviewer. Take a deep breath before you begin.",
+                },
+                Spellcheck = {
+                    model = 'gpt-4o',
+                    prompt =
+                    "Spellcheck the provided code and check its grammar. Pay special attention to strings, comments, and identifiers. Provide concise corrections without altering the logic or functionality of the code.",
+                    system_prompt =
+                    "You are an expert editor with a keen sense of grammar, spelling, and style. Take a deep breath before you start.",
+                },
+                ReviewCustom = {
+                    model = 'claude-3.5-sonnet',
+                    prompt =
+                    "Review provided code following best industry practices. Refactor code using the provided suggestions. After the review, made requested changes and submit the code.",
+                    system_prompt =
+                    "You are an experienced and meticulous software engineer with deep expertise in code quality, best practices, design patterns, and maintainability. Your primary objective is to perform thorough code reviews. When analyzing code, follow industry best practices, and provide constructive feedback to improve code quality. Identify issues with clarity and precision. Suggest practical improvements or alternatives. Offer a brief summary of strengths, weaknesses, and recommended changes. Remember: Take a deep breath before you start to review the code carefully and craft clear, actionable guidance.",
+                },
+                SecurityAudit = {
+                    model = 'o1',
+                    prompt =
+                    "Examine the provided code for potential security vulnerabilities and best-practice compliance. Provide clear remediation steps.",
+                    system_prompt =
+                    "You are a veteran security analyst with expertise in secure coding practices. Take a deep breath before you begin. Your goal is to thoroughly audit the provided code for security flaws, highlight potential attack vectors, and offer actionable steps to mitigate any risks. Always aim to maintain code functionality while enhancing overall security."
+                },
+                TestStrategy = {
+                    model = 'o1',
+                    prompt =
+                    "Review the provided code to suggest comprehensive testing strategies, identify missing test cases, and propose improvements for existing tests. Focus on coverage and reliability.",
+                    system_prompt =
+                    "You are a seasoned QA engineer and software tester. Take a deep breath before you begin. Your goal is to ensure robust test coverage by identifying gaps, proposing new test cases, and refining current testing methodologies."
+                },
+                RefactorHelper = {
+                    model = 'o1',
+                    prompt =
+                    "Refactor the provided code to enhance maintainability, readability, and extensibility. Avoid altering core functionality unless necessary.",
+                    system_prompt =
+                    "You are a meticulous refactoring expert. Take a deep breath before starting. Your focus is on reorganizing and simplifying the code to align with best practices while keeping the same output and behavior."
+                },
+                BetterNamings = {
+                    model = 'o1-mini',
+                    prompt =
+                    "Please propose clearer, more descriptive names for the following variables and functions. Follow established naming conventions and briefly explain why each suggested name is an improvement.",
+                    system_prompt =
+                    "You are an expert in code readability and naming conventions. Take a deep breath before you start. Your task is to evaluate existing variable and function names, then suggest improved alternatives that enhance clarity and maintain consistency."
+                }
+
+            },
             mappings = {
                 complete = {
                     detail = 'Use @<Tab> or /<Tab> for options.',
