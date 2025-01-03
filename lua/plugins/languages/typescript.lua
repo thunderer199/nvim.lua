0bd24d38209c543bf35a668cc8f3fc2e886463ae
Vlad Lashko <v.lashko@yahoo.com>
2025-01-03 16:14:27 +0000

    npm package info plugin

--- lua/plugins/languages/typescript.lua
+++ lua/plugins/languages/typescript.lua
@@ -6,6 +6,24 @@ return {
             debug = true,
             mode = "trouble", -- "quickfix" | "trouble"
         },
-    }
-}
+    },
+    {
+        "vuki656/package-info.nvim",
+        event = "BufReadPre",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
 
+        },
+        config = function()
+            require("package-info").setup({
+
+                hide_up_to_date = true,
+                autostart = false,
+                package_manager = "npm",
+            })
+            vim.api.nvim_create_user_command('PackageInfoToggle', function()
+                require("package-info").toggle()
+            end, {})
+        end,
+    },
+}
