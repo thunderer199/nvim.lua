9bb487348646f1f967a99f9c25bf992524825467
Vlad Lashko <v.lashko@yahoo.com>
2025-02-27 18:30:42 +0000

    CopyPath command: use relative path from git root when possible

--- lua/vlad/commands.lua
+++ lua/vlad/commands.lua
@@ -1,7 +1,23 @@
 vim.api.nvim_create_user_command("CopyPath", function()
-    local path = vim.fn.expand("%")
-    vim.fn.setreg("+", path)
-    vim.notify('Copied "' .. path .. '" to the clipboard!')
+	local util = require('vlad.util')
+	local git_cwd = util.get_git_cwd()
+	local base_dir = vim.fn.getcwd()
+	if git_cwd then
+		base_dir = git_cwd
+	end
+	local path = vim.fn.expand("%:p") -- Get absolute path
+
+	-- Ensure paths end with separator for proper replacement
+	if base_dir:sub(-1) ~= '/' then
+		base_dir = base_dir .. '/'
+	end
+
+	-- Remove the base directory from the path
+	if path:sub(1, #base_dir) == base_dir then
+		path = path:sub(#base_dir + 1)
+	end
+	vim.fn.setreg("+", path)
+	vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, {})
 
 vim.keymap.set('n', '<leader>cp', ':CopyPath<CR>')
