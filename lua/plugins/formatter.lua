053ab336364ab4aade7e4791d46bb55bcc9250c6
Vlad Lashko <v.lashko@yahoo.com>
2025-03-17 09:03:57 +0000

    update deps

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,6 +1,7 @@
 return {
     {
         'stevearc/conform.nvim',
+        commit = "80b57f6",
         config = function()
             local conform = require("conform")
 
@@ -74,12 +75,11 @@ return {
                     if args.count ~= -1 then
                         local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
                         range = {
-                            ["start"] = { args.line1, 0 },
+                            start = { args.line1, 0 },
                             ["end"] = { args.line2, end_line:len() },
                         }
                     end
-
-                    conform.format({ async = false, lsp_format = "fallback", range = range })
+                    require("conform").format({ async = true, lsp_format = "fallback", range = range })
                 end,
                 { range = true }
             )
