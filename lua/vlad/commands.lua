58ecf36b97bb8dbf482a20656b1a8cde6196a15b
Vlad Lashko <v.lashko@yahoo.com>
2025-04-30 14:24:27 +0000

    command to copy absolute path to clipboard

--- lua/vlad/commands.lua
+++ lua/vlad/commands.lua
@@ -6,7 +6,14 @@ vim.api.nvim_create_user_command("CopyPath", function()
 	vim.notify('Copied "' .. relative_path .. '" to the clipboard!')
 end, {})
 
+vim.api.nvim_create_user_command("CopyPathAbsolute", function()
+	local path = vim.fn.expand("%:p") -- Get absolute path
+	vim.fn.setreg("+", path)
+	vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
+
 vim.keymap.set('n', '<leader>cp', ':CopyPath<CR>')
+vim.keymap.set('n', '<leader>cP', ':CopyPathAbsolute<CR>')
 
 vim.api.nvim_create_user_command("BuffersClose", function()
 	local curr_buf = vim.api.nvim_get_current_buf()
