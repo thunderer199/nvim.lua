6ddd78b4edbb2fcfff1483857d7d14bd4c09c154
Vlad Lashko <v.lashko@yahoo.com>
2025-04-30 14:23:31 +0000

    feat: jump to line mapping

--- lua/plugins/jump.lua
+++ lua/plugins/jump.lua
@@ -2,8 +2,9 @@ return {
     'smoka7/hop.nvim',
     version = "*",
     keys = {
-        { '<leader>l', ':HopWord<CR>',  desc = "Hop to word" },
+        { '<leader>;', ':HopWord<CR>',  desc = "Hop to word" },
         { 's',         ':HopChar1<CR>', desc = "Hop to char" },
+        { '<leader>l', ':HopLineStart<CR>', desc = "Hop to line" },
     },
     opts = {},
 }
