91e2260a54def3e776025134172df712bace45a5
Vlad Lashko <v.lashko@yahoo.com>
2025-05-21 16:51:42 +0000

    new mason repos

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,8 +2,8 @@ return {
     'neovim/nvim-lspconfig',
     dependencies = {
         -- LSP Support
-        { 'williamboman/mason.nvim',          config = true },
-        { 'williamboman/mason-lspconfig.nvim' },
+        { 'mason-org/mason.nvim',          config = true },
+        { 'mason-org/mason-lspconfig.nvim' },
 
         'WhoIsSethDaniel/mason-tool-installer.nvim',
 
