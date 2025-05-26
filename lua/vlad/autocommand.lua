0e717a58b453a73cede17bbbf044e47732133bef
Vlad Lashko <v.lashko@yahoo.com>
2025-05-26 13:18:03 +0000

    feat(diagnostics): auto open diagnostics window when line has more than one diagnostic

--- lua/vlad/autocommand.lua
+++ lua/vlad/autocommand.lua
@@ -13,3 +13,16 @@ vim.api.nvim_create_autocmd(
     { "FocusLost", "InsertLeave" },
     { command = "silent! wa", group = "AutoSave" }
 )
+
+-- Show errors and warnings in a floating window
+vim.api.nvim_create_autocmd("CursorHold", {
+    callback = function()
+        -- get diagnostics for current buffer and line
+        -- if the are more than one, show them in a floating window
+        local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line('.') - 1 })
+        if #diagnostics <= 1 then
+            return
+        end
+        vim.diagnostic.open_float(nil, { focusable = false, source = "if_many" })
+    end,
+})
