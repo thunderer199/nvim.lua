0617b43db40fc4e3342283d5fcf4ecba30c35278
Vlad Lashko <v.lashko@yahoo.com>
2026-03-23 15:17:51 +0000

    refactor(plugins): refactor project-cli-commands config

--- lua/plugins/orchestrator.lua
+++ lua/plugins/orchestrator.lua
@@ -1,46 +1,40 @@
 return {
-    {
-        "dimaportenko/project-cli-commands.nvim",
+  {
+    "dimaportenko/project-cli-commands.nvim",
+    dependencies = {
+      "akinsho/toggleterm.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      { "<leader>p", "<cmd>Telescope project_cli_commands open<cr>", desc = "Project Commands" },
+      { "<leader>'", "<cmd>Telescope project_cli_commands running<cr>", desc = "Running Commands" },
+    },
+    opts = function()
+      local open_actions = require("project_cli_commands.open_actions")
+      local run_actions = require("project_cli_commands.actions")
 
-        dependencies = {
-            "akinsho/toggleterm.nvim",
-            "nvim-telescope/telescope.nvim",
+      return {
+        running_telescope_mapping = {
+          ["<C-c>"] = run_actions.exit_terminal,
+          ["<C-f>"] = run_actions.open_float,
+          ["<C-v>"] = run_actions.open_vertical,
+          ["<C-h>"] = run_actions.open_horizontal,
         },
-
-        -- optional keymap config
-        config = function()
-            local OpenActions = require('project_cli_commands.open_actions')
-            local RunActions = require('project_cli_commands.actions')
-
-            local config = {
-                -- Key mappings bound inside the telescope window
-                running_telescope_mapping = {
-                    ['<C-c>'] = RunActions.exit_terminal,
-                    ['<C-f>'] = RunActions.open_float,
-                    ['<C-v>'] = RunActions.open_vertical,
-                    ['<C-h>'] = RunActions.open_horizontal,
-                },
-                open_telescope_mapping = {
-                    { mode = 'i', key = '<CR>',  action = OpenActions.execute_script_vertical },
-                    { mode = 'n', key = '<CR>',  action = OpenActions.execute_script_vertical },
-                    { mode = 'i', key = '<C-h>', action = OpenActions.execute_script },
-                    { mode = 'n', key = '<C-h>', action = OpenActions.execute_script },
-                    { mode = 'i', key = '<C-i>', action = OpenActions.execute_script_with_input },
-                    { mode = 'n', key = '<C-i>', action = OpenActions.execute_script_with_input },
-                    { mode = 'i', key = '<C-c>', action = OpenActions.copy_command_clipboard },
-                    { mode = 'n', key = '<C-c>', action = OpenActions.copy_command_clipboard },
-                    { mode = 'i', key = '<C-f>', action = OpenActions.execute_script_float },
-                    { mode = 'n', key = '<C-f>', action = OpenActions.execute_script_float },
-                    { mode = 'i', key = '<C-v>', action = OpenActions.execute_script_vertical },
-                    { mode = 'n', key = '<C-v>', action = OpenActions.execute_script_vertical },
-                }
-            }
-
-            require('project_cli_commands').setup(config)
-
-            -- register telescope openers for project-cli-commands
-            vim.api.nvim_set_keymap("n", "<leader>p", ":Telescope project_cli_commands open<cr>", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap("n", "<leader>'", ":Telescope project_cli_commands running<cr>", { noremap = true, silent = true })
-        end
-    }
+        open_telescope_mapping = {
+          { mode = "i", key = "<CR>", action = open_actions.execute_script_vertical },
+          { mode = "n", key = "<CR>", action = open_actions.execute_script_vertical },
+          { mode = "i", key = "<C-h>", action = open_actions.execute_script },
+          { mode = "n", key = "<C-h>", action = open_actions.execute_script },
+          { mode = "i", key = "<C-i>", action = open_actions.execute_script_with_input },
+          { mode = "n", key = "<C-i>", action = open_actions.execute_script_with_input },
+          { mode = "i", key = "<C-c>", action = open_actions.copy_command_clipboard },
+          { mode = "n", key = "<C-c>", action = open_actions.copy_command_clipboard },
+          { mode = "i", key = "<C-f>", action = open_actions.execute_script_float },
+          { mode = "n", key = "<C-f>", action = open_actions.execute_script_float },
+          { mode = "i", key = "<C-v>", action = open_actions.execute_script_vertical },
+          { mode = "n", key = "<C-v>", action = open_actions.execute_script_vertical },
+        },
+      }
+    end,
+  },
 }
